d770a713d2d4bd377a93416616354dbedc961d90
fitrinanur <missfitrina@gmail.com>
2018-02-15 06:23:49 +0700

    installation laravel

---
+++ resources/views/script.blade.php
@@ -0,0 +1,8 @@
+<script src="{{ asset('assets/jquery/jquery-3.3.1.min.js') }}"></script>
+<script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+
+<script src="{{ asset('assets/dataTables/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/dataTables/js/dataTables.bootstrap.min.js') }}"></script>
+
+
+<script src="{{ asset('assets/validator/validator.min.js') }}"></script>
